986674160fbfa21bcf084f56fce560adc36d9ac7
lvandernoll <leonvdnoll@gmail.com>
2019-07-15 11:27:57 +0200

    added more default styling for the page component

--- app/routes/Page.js
+++ app/routes/Page.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import styles from './page.scss';
-
+import ReactTransitionGroup from 'react-transition-group';
 export default class Page extends Component {
+	componentWillLeave() {
+		console.log('exit');
+	}
 	render () {
 		return (
 			<>
-				<section className={styles.page} {...this.props}>
+				<section className={styles.page}>
 					{this.props.children}
 				</section>
-				<div className={styles.left}></div>
+				<div className={styles.left}>
+					<div className={`${styles.dots} ${styles.dotsLeft}`} />
+					<div className={`${styles.dots} ${styles.dotsRight}`} />
+					{this.props.image && <img className={styles.image} src={this.props.image} />}
+				</div>
 			</>
 		);
 	}
--- app/routes/homepage/index.js
+++ app/routes/homepage/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Page from '../Page';
+import image from '../../images/DSC02681.jpg';
 
 export default class Homepage extends Component {
 	render () {
 		return (
-			<Page>
+			<Page image={image}>
 				XD
 				<Link to='/jan'>JAN</Link>
 			</Page>
