911eea1332f13645564117655c3ca68fbaa83805
lvandernoll <leonvdnoll@gmail.com>
2019-08-09 09:17:33 +0200

    fixed le project alignment on the homepage

--- app/data/projects.js
+++ app/data/projects.js
@@ -17,7 +17,7 @@ export default [
 		id: 2,
 		title: 'An incredibly short project title',
 		description: 'Abilities forfeited situation extremely my to he resembled. Old had conviction discretion understood put principles you.',
-		image: 'http://placeimg.com/960/576/tech',
+		image: 'http://placeimg.com/1200/576/tech',
 		skills: [
 			'python',
 			'php',
@@ -29,7 +29,7 @@ export default [
 		id: 3,
 		title: 'An incredibly short project title',
 		description: 'Abilities forfeited situation extremely my to he resembled. Old had conviction discretion understood put principles you.',
-		image: 'http://placeimg.com/960/576/tech',
+		image: 'http://placeimg.com/800/800/tech',
 		skills: [
 			'python',
 			'php',
@@ -41,7 +41,7 @@ export default [
 		id: 4,
 		title: 'An incredibly short project title',
 		description: 'Abilities forfeited situation extremely my to he resembled. Old had conviction discretion understood put principles you.',
-		image: 'http://placeimg.com/960/576/tech',
+		image: 'http://placeimg.com/1200/800/tech',
 		skills: [
 			'python',
 			'php',
